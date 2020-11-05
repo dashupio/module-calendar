bb79876698cb8a44766c27b20bde2e59ee6a6d18
Dashup <git@dashup.io>
2020-11-05 14:27:24 +1000

    fix description

--- pages/calendar.ts
+++ pages/calendar.ts
@@ -20,7 +20,7 @@ export default class CalendarPage extends Struct {
    */
   get icon() {
     // return page type label
-    return 'fa fa-database';
+    return 'fa fa-calendar-week';
   }
 
   /**
@@ -57,7 +57,7 @@ export default class CalendarPage extends Struct {
    */
   get categories() {
     // return array of categories
-    return ['frontend'];
+    return ['View'];
   }
 
   /**
@@ -65,6 +65,6 @@ export default class CalendarPage extends Struct {
    */
   get description() {
     // return description string
-    return 'Page Descripton';
+    return 'Calendar view page';
   }
 }
